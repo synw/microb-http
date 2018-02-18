c7c0feed012a96ec1e17cfa28eb239affc02d6b2
Win <synwe@yahoo.com>
2018-02-18 15:50:47 +0000

    Handle pages titles

--- httpServer/data.go
+++ httpServer/data.go
@@ -1,34 +1,46 @@
 package httpServer
 
 import (
+	"bufio"
 	"github.com/synw/microb-http/types"
 	"github.com/synw/terr"
 	"html/template"
-	"io/ioutil"
+	"os"
 	"strings"
 )
 
 func getPage(domain string, url string, conn *types.Conn, edit_channel string) (*types.Page, *terr.Trace) {
 	filepath := "static/content/" + getFilepath(url)
-	content, tr := getContent(filepath)
+	title, content, tr := getContent(filepath)
 	if tr != nil {
 		tr := terr.Pass("httpServer.data.getPage", tr)
 		var p *types.Page
 		return p, tr
 	}
-	title := url
 	page := &types.Page{domain, url, title, template.HTML(content), conn, edit_channel}
 	terr.Debug(page)
 	return page, nil
 }
 
-func getContent(filepath string) (string, *terr.Trace) {
-	b, err := ioutil.ReadFile(filepath)
+func getContent(filepath string) (string, string, *terr.Trace) {
+	f, err := os.Open(filepath)
 	if err != nil {
 		tr := terr.New("httpServer.data.getContent", err)
-		return "", tr
+		return "", "", tr
 	}
-	return string(b), nil
+	scanner := bufio.NewScanner(f)
+	line := 1
+	title := ""
+	content := ""
+	for scanner.Scan() {
+		if line == 1 {
+			title = scanner.Text()
+		} else {
+			content = content + scanner.Text()
+		}
+		line = line + 1
+	}
+	return title, content, nil
 }
 
 func getFilepath(url string) string {
