bb6138220ea36aa67a61744d7617107050bd3f7b
Win <synwe@yahoo.com>
2018-04-04 13:14:06 +0000

    Add a csrf protection mechanism for forms

---
+++ csrf/csrf.go
@@ -0,0 +1,41 @@
+package csrf
+
+import (
+	"crypto/rand"
+	"fmt"
+	"github.com/synw/microb/libmicrob/redis"
+	"github.com/synw/terr"
+)
+
+func randToken() string {
+	b := make([]byte, 32)
+	rand.Read(b)
+	return fmt.Sprintf("%x", b)
+}
+
+func VerifyToken(token string) *terr.Trace {
+	_, err := redis.GetKey(token)
+	if err != nil {
+		tr := terr.New("csrf.VerifyToken", err)
+		return tr
+	}
+	return nil
+}
+
+func GetToken() (string, *terr.Trace) {
+	conn := redis.GetConn()
+	defer conn.Close()
+	token := randToken()
+	_, err := conn.Do("SET", token, "")
+	if err != nil {
+		tr := terr.New("csrf.GetToken", err)
+		return token, tr
+	}
+	ttl := 900 // the tokens will live for 15 minutes
+	_, err = conn.Do("EXPIRE", token, ttl)
+	if err != nil {
+		tr := terr.New("csrf.GetToken", err)
+		return token, tr
+	}
+	return token, nil
+}
